37bb73da5f0a3d3c5f005b6bd6c7e56621c7065b
Etch <etch@clearcutcoding.com>
2023-05-12 19:26:16 +0100

    install rector and fail script on error

--- src/ConfigLoader.php
+++ src/ConfigLoader.php
@@ -26,21 +26,21 @@ final class ConfigLoader
 
     private function locateConfigFile(string $path): ?string
     {
-        $dir_path = realpath($path);
+        $dirPath = realpath($path);
 
-        if (!is_dir($dir_path)) {
-            $dir_path = dirname($dir_path);
+        if (!is_dir($dirPath)) {
+            $dirPath = dirname($dirPath);
         }
 
         do {
-            $maybe_path = $dir_path . DIRECTORY_SEPARATOR . self::CONFIG_FILE;
+            $maybePath = $dirPath . DIRECTORY_SEPARATOR . self::CONFIG_FILE;
 
-            if (file_exists($maybe_path)) {
-                return $maybe_path;
+            if (file_exists($maybePath)) {
+                return $maybePath;
             }
 
-            $dir_path = dirname($dir_path);
-        } while (dirname($dir_path) !== $dir_path);
+            $dirPath = dirname($dirPath);
+        } while (dirname($dirPath) !== $dirPath);
 
         return null;
     }
--- src/Runner.php
+++ src/Runner.php
@@ -2,10 +2,11 @@
 
 namespace ClearCutCoding\SymfonyCiRunner;
 
+/**
+ * @psalm-suppress ForbiddenCode
+ */
 final class Runner
 {
-    private ConfigLoader $configLoader;
-
     private bool $noModsRun = false;
 
     // TERMINAL COLORS
@@ -15,27 +16,31 @@ final class Runner
     private const COL_BLUE = "\033[0;34m";
     private const COL_NC = "\033[0m"; // No Color
 
+    private readonly array $config;
+
+    private readonly string $vendorRoot;
+
     public function __construct(
-        ConfigLoader $configLoader
+        private readonly ConfigLoader $configLoader
     ) {
-        $this->configLoader = $configLoader;
+        $this->config = $this->configLoader->load();
+        $this->vendorRoot = $this->config['vendor-root'] ?? './';
     }
 
-    public function run($argv): void
+    public function run(array $argv): void
     {
         // if true, don't run any ci that will modify file.  e.g. useful for things like gitlab runner
         $this->noModsRun = in_array('--no-mods', $argv);
 
-        $config = $this->configLoader->load();
-
-        $this->phpcsfixer($config['phpcsfixer'] ?? false);
-        $this->lintyaml($config['lintyaml'] ?? false);
-        $this->linttwig($config['linttwig'] ?? false);
-        $this->phpcs($config['phpcs'] ?? false);
-        $this->phpunit($config['phpunit'] ?? false);
-        $this->phpmd($config['phpmd'] ?? false);
-        $this->phpstan($config['phpstan'] ?? false);
-        $this->psalm($config['psalm'] ?? false);
+        $this->rector($this->config['run']['rector'] ?? false);
+        $this->phpcsfixer($this->config['run']['phpcsfixer'] ?? false);
+        $this->lintyaml($this->config['run']['lintyaml'] ?? false);
+        $this->linttwig($this->config['run']['linttwig'] ?? false);
+        $this->phpcs($this->config['run']['phpcs'] ?? false);
+        $this->phpunit($this->config['run']['phpunit'] ?? false);
+        $this->phpmd($this->config['run']['phpmd'] ?? false);
+        $this->phpstan($this->config['run']['phpstan'] ?? false);
+        $this->psalm($this->config['run']['psalm'] ?? false);
     }
 
     private function begin(bool $do, string $title): bool
@@ -51,121 +56,150 @@ final class Runner
         return true;
     }
 
-    private function end(string $title): void
+    private function end(string $title, array $output, int $exitCode): bool
     {
+        echo implode(PHP_EOL, $output);
         echo "\n" . self::COL_GREEN . "END {$title}" . self::COL_NC . "\n";
+
+        if ($exitCode !== 0) {
+            exit;
+        }
+
+        return true;
     }
 
-    private function phpcsfixer(bool $do): void
+    private function rector(bool $do): bool
+    {
+        $title = 'RECTOR';
+
+        if ($this->noModsRun) {
+            echo "\n" . self::COL_RED . "IGNORE {$title}" . self::COL_NC . "\n";
+
+            return true;
+        }
+
+        if (!$this->begin($do, $title)) {
+            return true;
+        }
+
+        $cmd = "{$this->vendorRoot}vendor/bin/rector";
+        exec($cmd, $output, $exitCode);
+
+        return $this->end($title, $output, $exitCode);
+    }
+
+    private function phpcsfixer(bool $do): bool
     {
         $title = 'PHP-CS-FIXER';
 
         if ($this->noModsRun) {
             echo "\n" . self::COL_RED . "IGNORE {$title}" . self::COL_NC . "\n";
 
-            return;
+            return true;
         }
 
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
-        $cmd = 'PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix';
-        echo shell_exec($cmd);
+        $cmd = "PHP_CS_FIXER_IGNORE_ENV=1 {$this->vendorRoot}vendor/bin/php-cs-fixer fix";
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function lintyaml(bool $do): void
+    private function lintyaml(bool $do): bool
     {
         $title = 'YAML LINT';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
         $cmd = 'bin/console lint:yaml config';
-        echo shell_exec($cmd);
-        $cmd = 'bin/console lint:yaml src';
-        echo shell_exec($cmd);
+        exec($cmd, $output, $exitCode);
+        echo implode(PHP_EOL, $output);
 
-        $this->end($title);
+        $cmd = 'bin/console lint:yaml src';
+        exec($cmd, $output, $exitCode);
+
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function linttwig(bool $do): void
+    private function linttwig(bool $do): bool
     {
         $title = 'TWIG LINT';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
         $cmd = 'bin/console lint:twig src';
-        echo shell_exec($cmd);
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function phpcs(bool $do): void
+    private function phpcs(bool $do): bool
     {
         $title = 'PHPCS';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
-        $cmd = 'vendor/bin/phpcs --report=checkstyle --extensions=php src tests';
-        echo shell_exec($cmd);
+        $cmd = "{$this->vendorRoot}vendor/bin/phpcs --report=checkstyle --extensions=php src tests";
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function phpunit(bool $do): void
+    private function phpunit(bool $do): bool
     {
         $title = 'PHPUNIT';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
         $cmd = 'bin/phpunit --configuration phpunit.xml.dist';
-        echo shell_exec($cmd);
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function phpmd(bool $do): void
+    private function phpmd(bool $do): bool
     {
         $title = 'PHPMD';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
-        $cmd = 'vendor/bin/phpmd src,tests text controversial,unusedcode';
-        echo shell_exec($cmd);
+        $cmd = "{$this->vendorRoot}vendor/bin/phpmd src,tests text controversial,unusedcode";
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function phpstan(bool $do): void
+    private function phpstan(bool $do): bool
     {
         $title = 'PHPSTAN';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
         $cmd = 'php -d memory_limit=-1 vendor/bin/phpstan analyse src tests';
-        echo shell_exec($cmd);
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 
-    private function psalm(bool $do): void
+    private function psalm(bool $do): bool
     {
         $title = 'PSALM';
         if (!$this->begin($do, $title)) {
-            return;
+            return true;
         }
 
-        $cmd = 'vendor/bin/psalm --show-info=true';
-        echo shell_exec($cmd);
+        $cmd = "{$this->vendorRoot}vendor/bin/psalm --show-info=true";
+        exec($cmd, $output, $exitCode);
 
-        $this->end($title);
+        return $this->end($title, $output, $exitCode);
     }
 }
